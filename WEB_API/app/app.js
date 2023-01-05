1ab3137e32ae33494e38df5bdba46fcc857133f3
TrungNC <chitrung0895@gmail.com>
2023-01-05 08:13:24 +0700

    add swagger

--- WEB_API/app/app.js
+++ WEB_API/app/app.js
@@ -1,29 +1,29 @@
-const logger = require("morgan");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const helmet = require("helmet");
-const config = require("../config/config");
-const routes = require("./index.routes");
-const pageNotFoundMiddleware = require("./middlewares/not-found.middleware");
-const errorHandlerMiddleware = require("./middlewares/error-handler.middleware");
-const useragent = require("express-useragent");
-const swaggerJsdoc = require("swagger-jsdoc"),
-  swaggerUi = require("swagger-ui-express");
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const config = require('../config/config');
+const routes = require('./index.routes');
+const pageNotFoundMiddleware = require('./middlewares/not-found.middleware');
+const errorHandlerMiddleware = require('./middlewares/error-handler.middleware');
+const useragent = require('express-useragent');
+const swaggerJsdoc = require('swagger-jsdoc'),
+  swaggerUi = require('swagger-ui-express');
 
-const ev = require("express-validation");
+const ev = require('express-validation');
 // assign options
 ev.options({
   status: 422,
-  statusText: "Unprocessable Entity",
+  statusText: 'Unprocessable Entity',
 });
 
 const init = (app) => {
-  if (config.env === "development") {
-    app.use(logger("dev"));
+  if (config.env === 'development') {
+    app.use(logger('dev'));
   }
   // parse body params and attache them to req.body
-  app.use(bodyParser.json({ limit: "20mb" }));
-  app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+  app.use(bodyParser.json({ limit: '20mb' }));
+  app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 
   // secure apps by setting various HTTP headers
   app.use(helmet());
@@ -35,45 +35,45 @@ const init = (app) => {
   app.use(useragent.express());
   const options = {
     definition: {
-      swagger: "2.0",
+      swagger: '2.0',
       info: {
-        title: "LogRocket Express API with Swagger",
-        version: "0.1.0",
+        title: 'LogRocket Express API with Swagger',
+        version: '0.1.0',
         description:
-          "This is a simple CRUD API application made with Express and documented with Swagger",
+          'This is a simple CRUD API application made with Express and documented with Swagger',
         license: {
-          name: "MIT",
-          url: "https://spdx.org/licenses/MIT.html",
+          name: 'MIT',
+          url: 'https://spdx.org/licenses/MIT.html',
         },
         contact: {
-          name: "LogRocket",
-          url: "https://logrocket.com",
-          email: "info@email.com",
+          name: 'LogRocket',
+          url: 'https://logrocket.com',
+          email: 'info@email.com',
         },
       },
       servers: [
         {
-          url: "http://localhost:3000",
+          url: 'http://localhost:3000',
         },
       ],
     },
-    apis: ["./routes/*.js"],
+    apis: ['./routes/*.js'],
   };
 
   const specs = swaggerJsdoc(options);
   app.use(
-    "/api-docs",
+    '/api-docs',
     swaggerUi.serve,
-    swaggerUi.setup(specs, { explorer: true })
+    swaggerUi.setup(specs, { explorer: true }),
   );
   // parse validation error nicely
   if (!config.testing) {
-    app.use(require("./middlewares/checkToken.middleware"));
-    app.use(require("./middlewares/validationError.middleware"));
+    app.use(require('./middlewares/checkToken.middleware'));
+    app.use(require('./middlewares/validationError.middleware'));
   }
 
   // mount all routes on /api path
-  app.use("/api", routes);
+  app.use('/api', routes);
 
   // catch 404 and forward to error handler
   app.use(pageNotFoundMiddleware());
@@ -87,7 +87,7 @@ const init = (app) => {
     });
   }
 
-  console.log("Finished initialize app.");
+  console.log('Finished initialize app.');
 
   return app;
 };
